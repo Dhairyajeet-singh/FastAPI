44cbae0385d27c06d86f956de697df4fbf48a413
Dhairyajeet-singh <dhairya.jeet27@gmail.com>
2025-06-09 04:01:41 +0530

    C3 Path nd Query function

--- patient.py
+++ patient.py
@@ -3,6 +3,7 @@ import uvicorn, pydantic
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
 import json
+from fastapi import Path, Query
 
 app = FastAPI()
 @app.get('/')
@@ -31,4 +32,26 @@ def view():
         raise HTTPException(status_code=404, detail="No data found")
     return data
 
+@app.get('/patients/{id}')
+def view_by_id(id: str = Path(..., description="Enter patient number to view details"), example = "P001"):
+    data = load_data()
+    if id in data:
+        return data[id]
+    raise HTTPException(status_code = 404, detail = "patient not found, enter valid patient number")
 
+@app.get('/sort')
+def sort_patient(sort_by: str = Query(...,description="Enter the field to sort by", example="name"), order:str = Query('asc', description='sort in asc or desc order')):
+    valid_fileds = ['name','age','weight']
+    if sort_by not in valid_fileds:
+        raise HTTPException(status_code=400, detail=f"Invalid sort field. Valid fields are: {', '.join(valid_fileds)}")
+    
+    if order not in ['asc', 'desc']:
+        raise HTTPException(status_code=400, detail="Invalid order. Use 'asc' or 'desc'.")
+    
+    data = load_data()
+    if data == 0:
+        raise HTTPException(status_code=404, detail="No data found")
+    
+    sort_order = True if order == 'desc' else False
+    sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by,0), reverse = sort_order)
+    return sorted_data
