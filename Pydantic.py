729647e21859d4461abfdd5de960dd555cae780e
Dhairyajeet-singh <dhairya.jeet27@gmail.com>
2025-07-03 02:08:19 +0530

    updates: completed post and get methods

--- Pydantic.py
+++ Pydantic.py
@@ -1,11 +1,13 @@
-from pydantic import BaseModel
-from typing import List,Dict, Optional
+from pydantic import BaseModel, EmailStr, Field
+from typing import List,Dict, Optional, Annotated
 
 class patient(BaseModel):
 
-    name: str 
+    name: Annotated[str,Field(max_lenght=50, min_length=3, description="Name of the patient", example="John Doe")]
+
     age: int 
-    weight: float
+    email: EmailStr
+    weight: float = Field(gt=0, lt=120)
     married: Optional[bool] = None
     allergies: Optional[List[str]] = None
     contact_details: Dict[str,str]
@@ -16,12 +18,12 @@ def insert_patient(patient: patient):
     print(patient.weight)
     print("patient inserted")
 
-def insert_patient(patient: patient):
+def update_patient(patient: patient):
     print(patient.name)
     print(patient.age)
     print(patient.weight)
     print("patient updated")
 
-patient_info = {'name': 'john','age':20, 'weight': 75.4}
+patient_info = {'name': 'john','age':20, 'email': 'bhelu@gmail.com', 'weight': 75.4, 'contact_details': {'phone': '1234567890'}}
 patient1 = patient(**patient_info)
 insert_patient(patient1)
--- patient.py
+++ patient.py
@@ -1,11 +1,45 @@
 from fastapi import FastAPI
 import uvicorn, pydantic
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import HTTPException
 import json
 from fastapi import Path, Query
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
+
 
 app = FastAPI()
+
+class Patient(BaseModel):
+    id: Annotated[str, Field(..., max_length=10, min_length=3, description="Patient ID", example="P001")]
+    name: Annotated[str, Field(..., max_length=50, min_length=3, description="Name of the patient", example="John Doe")]
+    age: Annotated[int, Field(..., gt=0, lt=150, description="Age of the patient", example=30)]
+    city: Annotated[str, Field(..., max_length=50, description="City of the patient", example="New York")]
+    gender: Annotated[Literal['male','female','other'], Field(..., max_length=10, description = 'Your Gender (male, female, other)')]
+    height: Annotated[float, Field(..., gt=0, lt=300, description="Height of the patient in cm", example=175.5)]
+    weight: Annotated[float, Field(..., gt=0, lt=250, description="Weight of the patient in kg", example=70.5)]
+
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        height_in_m = self.height / 100  # Convert height from cm to m
+        bmi = round((self.weight/ (height_in_m**2)), 2)
+        return bmi
+    
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return "Underweight"
+        elif 18.5 <= self.bmi < 24.9:
+            return "Normal weight"
+        elif 25 <= self.bmi < 29.9:
+            return "Overweight"
+        else:
+            return "Obesity"
+        
+    
 @app.get('/')
 def msg():
     return {"message": "Patients information management system"}
@@ -24,6 +58,9 @@ def load_data():
     except json.JSONDecodeError:
         return 0
 
+def save_data(data):
+    with open('patients.json', 'w') as file:
+        json.dump(data, file)
 @app.get('/patients')
 
 def view():
@@ -55,3 +92,14 @@ def sort_patient(sort_by: str = Query(...,description="Enter the field to sort b
     sort_order = True if order == 'desc' else False
     sorted_data = sorted(data.values(), key = lambda x: x.get(sort_by,0), reverse = sort_order)
     return sorted_data
+
+
+@app.post('/create')
+def create_patients(patient: Patient):
+    data = load_data()
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail="Patient with this ID already exists.")
+    data[patient.id] = patient.model_dump(exclude=['id'])
+    save_data(data)
+
+    return JSONResponse(status_code=201, content={"message": "Patient created successfully"})
